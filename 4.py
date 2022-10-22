f529f3361ff71b6615cb0cd9ed3916d5cf0aea83
Aida <shakharova.aida@yandex.ru>
2022-10-22 13:04:51 +0600

    Задана натуральная степень k. Сформировать случайным образом список коэффициентов (значения от 0 до 100) многочлена и записать в файл многочлен степени k.

---
+++ 4.py
@@ -0,0 +1,24 @@
+# Задана натуральная степень k. 
+# Сформировать случайным образом список коэффициентов (значения от 0 до 100) многочлена 
+# и записать в файл многочлен степени k.
+
+from random import randint
+
+k = int(input('Введите натуральную степень k = '))
+array = []
+
+for i in range(k + 1):
+    array.append(randint(0, 100))
+result = []
+
+for i in range(k,-1,-1):
+    if array[i] != 0:
+        if i == 0:
+            result.append(f'{array[i]}')
+        elif i == 1:
+            result.append(f'{array[i]}*x')
+        else:
+            result.append(f'{array[i]}*x^{i}')
+
+with open ('file.txt', 'w') as data:
+    data.write(" + ".join(result) + " = 0")  
